04909d5a198ff149dd0863999b85b0c4ff109c46
getl0st <vladik.ko@hotmail.com>
2018-07-12 13:31:30 +0100

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿[assembly: System.Reflection.AssemblyCompany("getl0st")]
+[assembly: System.Reflection.AssemblyProduct("getl0st")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright (C) 2018")]
+[assembly: System.Reflection.AssemblyTitle("getl0st")]
