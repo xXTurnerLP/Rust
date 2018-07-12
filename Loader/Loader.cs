40d9c5e1e79d419c410e17025982f7f50b418d3b
getl0st <vladik.ko@hotmail.com>
2018-07-12 13:33:46 +0100

    Add files via upload

---
+++ Loader/Loader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Cheats_Class {
+	public class Loader {
+		public static void InitCheats() {
+			GameObject hackObj = new GameObject();
+			hackObj.AddComponent<HackManager>();
+			Object.DontDestroyOnLoad(hackObj);
+		}
+	}
+}
